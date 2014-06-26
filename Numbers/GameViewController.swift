0e7c0ac29191cae6884e25f8973eaf25661ffd35
Gustavo Costa <GustavoCosta@MacBook-Air-de-Gustavo.local>
2014-06-26 21:05:14 +0100

    First Commit - Gustavo

---
+++ Numbers/GameViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GameViewController.swift
+//  Numbers
+//
+//  Created by Gustavo Arthur da Costa on 26/06/14.
+//  Copyright (c) 2014 Gustavo Arthur da Costa. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
---
+++ Numbers/OptionsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  OptionsViewController.swift
+//  Numbers
+//
+//  Created by Gustavo Arthur da Costa on 26/06/14.
+//  Copyright (c) 2014 Gustavo Arthur da Costa. All rights reserved.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
