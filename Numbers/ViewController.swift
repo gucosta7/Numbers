7a56062f3a3a880571299e39cef7e87a713a4f6e
Gustavo Costa <GustavoCosta@MacBook-Air-de-Gustavo.local>
2014-06-26 20:31:39 +0100

    Initial Commit

---
+++ Numbers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Numbers
+//
+//  Created by Gustavo Arthur da Costa on 26/06/14.
+//  Copyright (c) 2014 Gustavo Arthur da Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
