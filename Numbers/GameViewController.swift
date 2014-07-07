da009ae520f5602a0d06a7e854a0ac3941bfa2ee
Gustavo Costa <GustavoCosta@MacBook-Air-de-Gustavo.local>
2014-07-07 22:45:36 +0100

    gustavo foda 0707

--- Numbers/GameViewController.swift
+++ Numbers/GameViewController.swift
@@ -11,6 +11,17 @@ import UIKit
 class GameViewController: UIViewController {
 
     @IBOutlet var counterLabel : UILabel!
+    
+    @IBOutlet var btnGrid11 : UIButton!
+    @IBOutlet var btnGrid12 : UIButton!
+    @IBOutlet var btnGrid13 : UIButton!
+    @IBOutlet var btnGrid21 : UIButton!
+    @IBOutlet var btnGrid22 : UIButton!
+    @IBOutlet var btnGrid23 : UIButton!
+    @IBOutlet var btnGrid31 : UIButton!
+    @IBOutlet var btnGrid32 : UIButton!
+    @IBOutlet var btnGrid33 : UIButton!
+    @IBOutlet var btnGrid41 : UIButton!
 
     
     //var timer : NSTimer = NSTimer.init()
@@ -19,8 +30,7 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //timer.fire()
+        btnGrid11.setTitle("Test", forState: UIControlState.Normal)
 
     }
 
@@ -29,11 +39,7 @@ class GameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func update() {
-        
-        //counterLabel.text = timer.timeInterval
-        
-    }
+    
     
     
     
