ea66ecc7c5d7904c4f1e8a069c71981939a80571
Ulisses Alves Rodrigues <ulisses.a.ro@gmail.com>
2014-08-21 15:33:58 +0100

    Times Changed - Last pop up fixed

--- Numbers/OptionsViewController.swift
+++ Numbers/OptionsViewController.swift
@@ -129,7 +129,7 @@ class OptionsViewController: UIViewController, UIApplicationDelegate, FBLoginVie
             }
             let level1action = UIAlertAction(title: level1, style: .Default) { action in
                 level = 1
-                
+                self.updateLevel(level)
             }
             let level2action = UIAlertAction(title: level2, style: .Default) { action in
                 level = 2
